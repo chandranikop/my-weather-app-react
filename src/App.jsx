972aa361fec2df023af368bb5efb05333cd8125c
Chandra Niko P <nikochandra3@gmail.com>
2026-01-21 01:35:39 +0700

    kekurangan konfigurasi tailwind, dan menambah teknologi lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1>My Weather App using React.JS</h1>;
+  return (
+    <div className="min-h-screen bg-linear-to-br from-blue-400 to-indigo-900 flex flex-col items-center justify-center p-4 font-sans text-white">
+      <h1 className="text-4xl">My Weather App using React.JS</h1>
+    </div>
+  );
 };
 
 export default App;
